8801c04f1cac219fb16b6b05e84d38b45fe369dd
ThiagoNascBarros <thiago171nasciment@gmail.com>
2024-11-13 08:41:14 -0300

    feat: Agendamento

--- public/js/agendamento.js
+++ public/js/agendamento.js
@@ -1,15 +1,17 @@
 // Inicializa o Flatpickr para o campo de Data
 flatpickr("#data", {
-    dateFormat: "d/m/Y",  // Formato de data
-    minDate: "today",     // Impede a seleção de datas passadas
-    locale: "pt"          // Define o idioma para português
-  });
+  dateFormat: "d/m/Y",  // Formato de data
+  minDate: "today",     // Impede a seleção de datas passadas
+  locale: "pt"          // Define o idioma para português
+});
 
-  // Inicializa o Flatpickr para o campo de Hora
-  flatpickr("#hora", {
-    enableTime: true,         // Ativa o seletor de hora
-    noCalendar: true,         // Desativa o calendário, deixando apenas a seleção de hora
-    time_24hr: true,          // Exibe hora no formato 24h
-    dateFormat: "H:i",        // Formato de hora (hora:minutos)
-    locale: "pt"              // Define o idioma para português
-  });
+// Inicializa o Flatpickr para o campo de Hora
+flatpickr("#hora", {
+  enableTime: true,         // Ativa o seletor de hora
+  noCalendar: true,         // Desativa o calendário, deixando apenas a seleção de hora
+  time_24hr: true,          // Exibe hora no formato 24h
+  dateFormat: "H:i",        // Formato de hora (hora:minutos)
+  locale: "pt",             // Define o idioma para português
+  minTime: "09:00",         // Limita a hora mínima para 09:00
+  maxTime: "18:00"          // Limita a hora máxima para 18:00
+});
